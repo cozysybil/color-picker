3776b52fc101805c30d84feb589ff873e9a3cbb5
Cozysybil <cozysybil@gmail.com>
2020-07-23 17:19:57 +0700

    add fullscreen mode

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
 import React, { useState } from "react";
+import { Button } from "react-bootstrap";
+import { FullScreen, useFullScreenHandle } from "react-full-screen";
 
 import Code from "./pages/Code/Code";
 import Setcode from "./pages/Setcode/Setcode";
-
 const App = () => {
     const [code, setCode] = useState(null);
 
-    return code ? (
-        <Code setupCode={() => setCode(null)} codeSetting={code} />
-    ) : (
-        <Setcode showCode={(codeSetting) => setCode(codeSetting)} />
+    const handle = useFullScreenHandle();
+
+    return (
+        <FullScreen handle={handle}>
+            <div style={{ backgroundColor: "#fff", minHeight: "100vh" }}>
+                <Button
+                    variant="dark"
+                    onClick={handle.active ? handle.exit : handle.enter}
+                    size="sm"
+                    block
+                >
+                    {handle.active ? "Exit fullscreen" : "Enter fullscreen"}
+                </Button>
+                {code ? (
+                    <Code setupCode={() => setCode(null)} codeSetting={code} />
+                ) : (
+                    <Setcode showCode={(codeSetting) => setCode(codeSetting)} />
+                )}
+            </div>
+        </FullScreen>
     );
 };
 
